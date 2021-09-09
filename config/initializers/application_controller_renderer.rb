3e997c503e3c71d051cdfc7577e48ceac1eee8ca
KouOsada <kouosada.0930@gmail.com>
2021-09-09 08:17:17 +0000

    [Add]投稿一覧画面

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '37d5300661eaba282221c6b1d5ab4728d141fa91a8388f2c66104429b8b250e5e17627eaff547ae27facb0b03a2460183a999ea5efa952210dc983bb6d2a2d9b'
